b49d7014ca2b2e5fcf13c0d7b71cee2ffa18fb28
Kira <kira666@infierno.es>
2019-05-08 21:23:34 +0200

    Clase 9

---
+++ 09_Elementos/pie.php
@@ -0,0 +1,7 @@
+<footer class="main_footer">
+        <p>José Velarde - ©Lechero´´SA</p>
+        <a href="mailto://ElLechero@hotmail.es">Enviar correo</a>
+        <a>rel= "external"</a>
+    </footer>
+</body>
+</html>
