d1cd5052ca73cfb3583e6049f9d3802dc92b07cb
Kira <kira666@infierno.es>
2019-05-14 21:26:07 +0200

    1

--- Ejemplo1/pie.php
+++
@@ -1,9 +0,0 @@
-<footer class="mainfooter"> 
-        <address> <p>Escrito por:  Jose © <!-- copyrght se escribe alt+0+1+6+9 ó &copy) todos a la vez o lo saco de word o lo copio y pego de donde sea --> </p> </address>
-       
-        <a href="mailto://lechero666@gmail.com" rel="Author">Contacto</a> <!-- mailto/ etiqueta para enlazar al correo se parte de la etiqueta a -->
-    </footer>
-        
-</body>
-
-</html>
