bbfa601f42e4a0f090b49ae4063c10bc2a8d42ea
Kira <kira666@infierno.es>
2019-05-17 21:20:41 +0200

    conjava

---
+++ 09_Elementos/js/app.js
@@ -0,0 +1,44 @@
+import dialogPolyfill from "../../node_modules/dialog-polyfill/index.js"
+
+function main() {
+    document.querySelector("#btn-Info")
+    .addEventListener("click", mostrar)
+    document.querySelector("#btn-cerrar")
+    .addEventListener("click", cerrar)
+
+    function mostrar() {
+        document.querySelector("#dlg-sample").open =true
+    }
+    function cerrar() {
+        document.querySelector("#dlg-sample").open =false
+    }
+}
+
+function main2() {
+    let modal = document.querySelector("#dlg-sample")
+    
+    dialogPolyfill.registerDialog(modal);
+    // Now dialog acts like a native <dialog>.
+    modal.showModal();
+
+    document.querySelector("#btn-Info")
+    .addEventListener("click", onclick)
+    document.querySelector("#btn-cerrar")
+    .addEventListener("click", onclick)
+
+    function onclick() {
+        if (!modal.open) (modal.open = false)
+        modal.open = !modal.open
+        if (!modal.open) {
+        modal.showModal();
+       }
+    }
+}
+
+function canvas () {
+    let c = document.querySelector("#cnv-sample").getContext("2d")
+    c.fillRect(20, 20, 50, 75);
+}
+
+document: addEventListener("DOMContentLoaded", main2)
+document: addEventListener("DOMContentLoaded", canvas)
