2a288fa14ea3c44ba42f63c63a5786329f3279fd
mickey <mmouse@hotmail.com>
2019-05-29 11:04:56 +0200

    Clase actualizada

--- 12_Intro_js/06_referencias.js
+++ 12_Intro_js/06_referencias.js
@@ -1,61 +1,75 @@
-"use strict"
 
+'use strict'
 
-//valores primitivos
-// (number, string, boolean)
-
-
+// Valores primitivos 
+// (number, string, boolean, undefined)
 
 let a = 2
-let b = a // asignacion de valor
-console.log("A crear b", a, b)
+let b = a  // aisgnación de Valor 
+console.log('Al crear b', a, b)
 b = b * b
 a = a / 2
-console.log( "valor de b", b) // 4
-console.log("valor de a", a) // 1
+console.log('Valor de b', b) // 4
+console.log('Valor de a', a) // 1
 
 b = a
-console.log("valor de b", b)
+console.log('Valor de b', b) 
 
 
-//valores referenciados
-let p1 = {nombre: "Pepe", edad: 34}
-let p2 = p1
-p1.nombre = "Juan"
+// Valores referenciados
+
+let p1 = {nombre : 'Pepe', edad: 34}
+let p2 = p1 // asignación de referencia
+p1.nombre = 'Juan'
 console.log(p1) //
 console.log(p2)
 
 
-/* let a1 = {nombre: "Raquel", edad: 24} */
+let a1 = {nombre : 'Raquel', edad: 34, saldo : 1000}
+let a2 = {nombre : 'Maria', edad: 34, saldo: 1200}
+let a3 = {nombre : 'Juan', edad: 34 , saldo: 1300}
 
-function inscribirEnJS() {
-    a1.curso = "JS"
+
+function inscribirMal() {
+    a1.curso = 'JS'
 }
-
-let a1 = {nombre: "Raquel", edad: 24}
-let a2 = {nombre: "Maria", edad: 29}
-let a3 = {nombre: "Miguel", edad: 19}
 
 function inscribirEnJS(alumno) {
     if (alumno) {
-        alumno.curso = "JS"
+        alumno.curso = 'JS'
     }
-}    
-inscribirEnJS(a1)
+}
+
+
+function inscribir(alumno, curso = 'JS') {
+    if (alumno) {
+        alumno.curso = curso
+    }
+}
+
+
+/* inscribirEnJS(a1) // paso por referencia
 inscribirEnJS(a2)
-inscribirEnJS(a3)
+inscribirEnJS(a3) */
+
+inscribir(a1, 'HTML') // paso por referencia
+inscribir(a2)
+inscribir(a3)
 
 console.log(a1)
 console.log(a2)
 console.log(a3)
 
-let saldo = 1000
-
-
-
-function invertir(x){
-    x = x * 2
-    return x
+function invertir(x = 0) {
+    let z = Math.random()
+    let rendimiento =  (x * z) / 10
+    return (x + rendimiento).toFixed(2)
 }
-saldo = invertir(saldo)
-console.log(saldo)
+
+a1.saldoNew = invertir(a1.saldo) // Paso por valor
+a2.saldoNew = invertir(a2.saldo) // Paso por valor
+a3.saldoNew  = invertir(a3.saldo) // Paso por valor
+console.log(a1)
+console.log(a2)
+console.log(a3)
+
--- 12_Intro_js/07_casting.js
+++ 12_Intro_js/07_casting.js
@@ -1,51 +1,73 @@
+'use strict'
 
-"use strict"
-
-//Operaciones con Numbers
+// Operaciones con Numbers
 let x = 2
 let y = 3
 let z = x * y
 
-//Operaciones con numbers y string
-y = "3"
-z = x * y //Durante la numeracion Number (Y)
-x = "5"
+// Operaciones con Numbers y Strings
+y = '3'
+z = x * y // Durante la operación Number(y)
+x = '5'
 z = x * y
 
+console.log(z, typeof z) // 6// number
+console.log(x, typeof x) // string
+console.log(y, typeof y) // string
 
-console.log(z, typeof z) //6//Number
-console.log(x, typeof x) //string
-console.log(y, typeof y) //string
-
-/* sobrecarga del simbolo +
+/* Sobrecarga del simbolo +
 +3
-2+2 
-"hola" + "pepe" */
+"Hola " + "Pepe"
+2+2
+*/
 
-let w = x + y //No da 8, da "53"
+let saludo = "Hola, " + "Pepe"
+
+let w = x + y // No da 8, da '53'
 console.log(w, typeof w)
 
 x = 23
-y = "20"
-w = x + y //da 2320
+y = '20'
+w = x + y // da '2320'
 w = w * 1
 console.log(w, typeof w)
 
 /**
- * NOT !
+ * not !
  * OR ||
- * and &&
+ * AND &&
  */
 
-
-function sumar (a, b) {
-    if (typeof a != "number" || typeof b != "number") {
-        return "No se puede sumar"
-    }
-    let r = a + b
+function sumar (a = 0, b = 0) {
     return a + b
 }
 
-console.log(sumar (2, 2))
-console.log(sumar (2, 0))
-console.log(sumar (2, "5"))
+function sumar (a = 0, b = 0) {
+    if ( isNaN(a) || isNaN(b) 
+    || typeof a == 'boolean' || typeof b == 'boolean'
+    || typeof a == 'object' || Array.isArray(b)
+    ) {
+        return 'No se puede sumar'
+    }
+    // let r = Number(a) + Number(b)
+    // let r = parseFloat(a) + parseFloat(b)
+    let r = +a + +b
+    return 'La suma es ' + r
+}
+
+console.log(sumar())
+console.log(sumar(undefined, undefined))
+console.log(sumar(5))
+console.log(sumar(2,2))
+console.log(sumar(2,0))
+console.log(sumar(0,2))
+console.log(sumar(2,'5'))
+console.log(sumar('2','5'))
+console.log(sumar(2,'Pepe'))
+console.log(sumar('Juan','Pepe'))
+console.log(sumar(2, true))
+console.log(sumar(2, false))
+console.log(sumar(true, false))
+console.log(sumar({}, {}))
+console.log(sumar([], []))
+console.log(sumar([23, 56], [78, 23]))
