60be15835ab649ce7181a9a8fe0f3e4d4ed39ebe
Kira <kira666@infierno.es>
2019-05-29 21:23:11 +0200

    Clase nueva

--- 12_Intro_js/08_booleans.js
+++ 12_Intro_js/08_booleans.js
@@ -78,3 +78,54 @@ let b = '4'
 
 console.log(a == b) // true
 console.log(a === b) // false
+
+let nota = 0
+
+
+/* if (!nota) */
+
+if (nota != true){
+    console.log("Has sacado la nota minima")
+}           
+
+let b1 = Boolean (nota)
+b1 =!!nota
+
+/* let edad = 16
+if (edad < 18) {
+    console.log("Eres menor de edad")
+    
+}    
+    else if (edad < 38) {
+        console.log("Eres joven")
+    }
+    else if(edad < 50){
+        console.log("Empieza a tener cuidado")
+    }
+    else{
+        console.log("Lo siento por ti")
+    }
+ */
+
+
+function comprobarEdad (edad) {
+    let i
+    let mensajes = [
+        `Eres menor de edad`,
+        `Eres joven`,
+        `Empieza a tener cuidado`,
+        `Lo siento por ti`
+    ]
+    if (edad < 18){
+      i =0  
+    } else if (edad < 38){
+        i = 1
+    } else if(edad < 50){
+        i = 2
+    }else {
+        i = 3
+    }
+    console.log(`Con ${edad} años`, mensajes [i])    
+}
+
+let it 
