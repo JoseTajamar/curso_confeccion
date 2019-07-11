72aa178ca2234f26abe775e96ad282e2c0639d93
CursosTardes <CursosTardes@tic.loc>
2019-07-11 19:51:49 +0200

    nuevo

--- JS_OOP/26_asincronia.js
+++ JS_OOP/26_asincronia.js
@@ -1,6 +1,4 @@
-
-
-function leerDatosAsinc(i, timer = 1000, callback) {
+/* function leerDatosAsinc(i, timer = 1000, callback) {
     return setTimeout(
         () => {
             let datos
@@ -18,4 +16,36 @@ leerDatosAsinc(
     1, 2000, (datos) => 
     console.log(datos.data.toUpperCase())
 )
+console.log('Leyendo los datos') */
+
+
+const respuestas = [
+    'Hola',
+    'Aleco virgen',
+    'Mandy mi AMoool',
+    'CUÑAOOOO',
+    'Hola'
+]
+
+
+function leerDatosAsinc(i, timer = 1000, callback) {
+    setTimeout(() => {
+            console.log(`lectura de datos número ${i}`)
+            callback(respuestas [i])
+        }, timer)
+} 
+
+
+leerDatosAsinc ( 1, 2000, (datos) => {
+    console.log(datos)
+    leerDatosAsinc(2, 3000,(datos) => {
+        console.log(datos)
+        leerDatosAsinc(3, 2000, (datos) =>{
+            console.log(datos)
+            leerDatosAsinc(4, 1000, (datos) => {
+                console.log(datos)
+            })
+        })
+    })
+})
 console.log('Leyendo los datos')
