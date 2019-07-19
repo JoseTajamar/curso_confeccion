fcd5c87d602345c83b62e0ef6dbcbd6eff1298ea
CursosTardes <CursosTardes@tic.loc>
2019-07-19 18:51:36 +0200

    Trabajo examen

---
+++ Tarea_programa/Readme.md
@@ -0,0 +1,7 @@
+##Crear un programa capaz de analizar una frase:
+
+-número de palabras
+-longitud media de las palabras
+-veces cada letra
+-frecuencia cada letra
+-Incorporar un interface Web de entrada y salida
---
+++ Tarea_programa/programa.js
@@ -0,0 +1,38 @@
+/**
+ * @description: Funcion de analizar el numero de palabras
+ * 
+ */
+
+/* function ContarPalabras(numero){  
+
+    for (let i = 0; i < s.length(); i++) {
+        const element = array[i];
+        if (Character.ContarPalabras(s.charAt(i)) && i != finDeLinea) {
+            palabra = true;
+            // Si el char no es una letra y aún hay más letras,
+            // el contador continua.
+        } else if (!Character.ContarPalabras(s.charAt(i)) && palabra) {
+            ContarPalabras++;
+            palabra = false;
+            // última palabra de la cadena; si no termina con una no letra ,
+        } else if (Character.ContarPalabras(s.charAt(i)) && i == finDeLinea) {
+            ContarPalabras++;
+        }
+    }
+    return ContarPalabras;
+}
+console.log("Palabras : " + ContarPalabras("Hola mundo"))
+ */
+/* function cuantasVecesAparece(cadena, character){
+    var indices = [];
+    for(var i = 0; i < cadena.length; i++) {
+      if (cadena[i].toLowerCase() === character) indices.push(i);
+    }
+      return indices.length;
+  }
+  
+  var coincidencias = cuantasVecesAparece("Cuantas 'E' aparecen en esta cadena.", "e");
+  
+  document.getElementById("resultado").innerHTML = coincidencias;
+  console.log('') */
+
