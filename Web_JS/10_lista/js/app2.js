e4a029ee0f793a816199e065644c226e948d22e1
CursosTardes <CursosTardes@tic.loc>
2019-07-26 19:55:42 +0200

    Nuevo JS

--- Web_JS/10_lista/js/app2.js
+++ Web_JS/10_lista/js/app2.js
@@ -4,19 +4,31 @@ export class App {
 
     constructor() {
         console.log('Cargada app') 
-        this.setAnimales = new Set()
 
+        // servicio que maneja localStorage
         this.store = new ArrayStorage('animales')
-
+                
+        // Nodos del DOM    
         this.inNew = document.querySelector('#in-new')
         this.outLista = document.querySelector('#out-lista')
 
+        // Definición de manejadores de eventos
         this.inNew.addEventListener('change',
-            this.onChange.bind(this))
+            this.onChangeInput.bind(this))
 
+        // Inicialización de un Set, 
+        // en su caso desde localStorage
+        this.setAnimales = this.store.get()
+            ? new Set(this.store.get()) : new Set()
+        this.render()
+
+        console.log(this)
     }
     
-    render() {
+    renderOld() {
+        if (!this.setAnimales.size) {
+            return
+        }
         let txtHtml = ''
         this.setAnimales.forEach( animal =>
             txtHtml += `<li>${animal}</li>`)
@@ -27,16 +39,60 @@ export class App {
         this.outLista.appendChild(nodoLista)
     }
 
-    onChange() {
+    render() {
+        let html = '' 
+        html += '<ul>'
+        this.setAnimales.forEach(item => html += 
+            `<li>
+                <span class="texto" data-id="${item}">${item}</span>
+                <span class="btn btn-editar" data-id="${item}">️✍️</span>  
+                <span class="btn btn-borrar" data-id="${item}">🗑️</span>
+            </li>`)
+        html += '</ul>'
+        this.outLista.innerHTML = html
+
+        this.aBtnBorrar = document.querySelectorAll('.btn-borrar')
+        this.aBtnEditar = document.querySelectorAll('.btn-editar')
+        this.aTextos = document.querySelectorAll('.texto')
+
+        this.aBtnBorrar.forEach(btn => btn.addEventListener('click', 
+            this.onBorrar.bind(this)))
+        this.aBtnEditar.forEach(btn => btn.addEventListener('click', 
+            this.onEditar.bind(this)))
+        this.aTextos.forEach(txt => txt.addEventListener('blur', 
+            this.onChange.bind(this)))
+    }
+
+    save() {
+            this.store.set(Array.from(this.setAnimales))
+        }
+
+    onChangeInput() {
         if (this.inNew) {
             this.setAnimales.add(this.inNew.value)
-            this.save()
             this.inNew.value = ''
+            this.save()
             this.render()
         }
     }
 
-    save() {
-        this.store.set(this.setAnimales)
+    onBorrar(ev) {
+        this.setAnimales.delete(ev.target.dataset.id)
+        this.save()
+        this.render()
     }
+
+    onEditar(ev) {
+        ev.target.previousElementSibling.contentEditable = true
+    }
+
+   onChange(ev) {
+    ev.target.contentEditable = false
+    let a = [...this.setAnimales]
+    a[a.indexOf(ev.target.dataset.id)] = ev.target.textContent
+    this.setAnimales = new Set(a)
+    this.save()
+    this.render()
+   }
+   
  }
