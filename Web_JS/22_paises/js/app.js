7f3aa7ae7425e52c63c367dfa0bc360372fedba5
CursosTardes <CursosTardes@tic.loc>
2019-09-06 19:04:34 +0200

    Paises selector

---
+++ Web_JS/22_paises/js/app.js
@@ -0,0 +1,44 @@
+const world = 'https://restcountries.eu/rest/v2/all' 
+
+// Nodos del DOM
+let regionsList = document.querySelector("#continente")
+let countriesList = document.querySelector("#pais")
+
+
+// Asociación de manejadores de eventos
+
+regionsList.addEventListener("change", newRegionSelection)
+countriesList.addEventListener("change", newCountrySelection)
+
+ // Funciones manejadoras de eventos
+function newRegionSelection(ev) {
+  
+}
+
+function newCountrySelection(ev) {
+  CountryInfo(ev.target.value)
+}
+
+fetch(world)
+.then(response => response.json())
+.then(data => init(data))
+
+function init(paisData) {
+  countries = paisData
+  let opciones = ""
+ 
+  countries.forEach(pais => opciones+=`<option value="${pais.name}">${pais.name}</option>`)
+
+  countriesList.innerHTML = opciones
+}
+
+function CountryInfo(Info) {
+  let countryData = countries.find(country => country.name == Info)
+  document.querySelector("#flag-container img").src = countryData.flag;
+  document.querySelector("#capital").innerHTML = countryData.capital
+  document.querySelector("#region").innerHTML = countryData.region
+  document.querySelector("#subregion").innerHTML = countryData.subregion
+  document.querySelector("#languaje").innerHTML = countryData.demonym
+  document.querySelector("#nativo").innerHTML = countryData.nativeName
+  document.querySelector("#area").innerHTML = `${countryData.area} KM²`
+}
